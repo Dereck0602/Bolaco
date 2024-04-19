bda9d788710d00afe30906fb91e321c69b86d7cf
Yixin <52696292+Dereck0602@users.noreply.github.com>
2024-04-20 00:29:42 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@ export CUDA_VISIBLE_DEVICES=0
 model="/path/to/model"
 python main_bbo.py \
     --model ${model} \
-    --prune_method lord \
     --remain_ratio 0.8 \
     --nsamples 1024 \
     --save_model \
